c35bdc75445d5aa52a5a104ff1fdc500cc133aa5
EvatheGreat <116327580+EvatheGreat@users.noreply.github.com>
2023-02-16 19:31:53 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 var canvas = new fabric.Canvas('myCanvas');
  var x= document.getElementById("myAudio");
-
 function new_image(){
-	fabric.Image.fromURL('BirthdayImage.jpg',function(Img){
+	fabric.Image.fromURL('https://thumbs.dreamstime.com/b/happy-birthday-card-boy-blue-cupcake-48149079.jpg',function(Img){
         block_image_object=Img;
-        
         block_image_object.scaleToWidth(700);
         block_image_object.scaleToHeight(510);
         block_image_object.set({
@@ -14,7 +12,6 @@ function new_image(){
         canvas.add(block_image_object);
     })	
 }
-
 function playSound(){
 	x.play();
 }
